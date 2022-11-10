2b69b03a10a18f453facd7d2595ee663b76e2ee5
unknown <jawanistanford@students.ptechnyc.org>
2022-11-09 21:03:00 -0500

    code hold the main process of the overall code

--- Searching/search_buy.py
+++ Searching/search_buy.py
@@ -1,3 +1,13 @@
-from selenium import webdrivers
+import Searching.constants
+import os
+from selenium import webdriver
 
-class Booking(webdrivers.Chrome)
+class Searching(webdriver.Chrome):
+    def __init__(self, driver_path=r"C:\SeleniumDrivers"):
+        self.driver_path = driver_path
+        os.environ['PATH'] += self.driver_path
+        super(Searching, self).__init__()
+
+
+    def main_page(self):
+        self.get(Searching.constants.BASE_URL)
