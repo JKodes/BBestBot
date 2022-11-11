65e3bea0048862fcc90f013981592fc42c0e17bc
unknown <jawanistanford@students.ptechnyc.org>
2022-11-11 01:35:17 -0500

    add teardown boolean to class calling

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ import searching.search_buy
 #run = searching.search_buy.Searching()
 #run.main_page()
 
-with searching.search_buy.Searching() as done:
+with searching.search_buy.Searching(teardown=True) as done:
     done.main_page()
