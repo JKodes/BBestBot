58f09356e45d09fe0212df90f4d6a62612fb6bc2
unknown <jawanistanford@students.ptechnyc.org>
2022-11-13 12:38:08 -0500

    item is now automatically typed in the search

--- run.py
+++ run.py
@@ -5,5 +5,6 @@ import searching.search_buy
 #run = searching.search_buy.Searching()
 #run.main_page()
 
-with searching.search_buy.Searching(teardown=True) as done:
+with searching.search_buy.Searching() as done:
     done.main_page()
+    done.type_item_of_choice('Air Conditioner')
