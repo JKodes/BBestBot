ac6d1776d6a4e66650369be23933383e9d119077
unknown <jawanistanford@students.ptechnyc.org>
2022-11-13 22:42:22 -0500

    minor changes

--- Searching/search_buy.py
+++ Searching/search_buy.py
@@ -4,17 +4,17 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class Searching(webdriver.Chrome):
-    def __init__(self, driver_path=r"C:\SeleniumDrivers", ):   #,teardown=False
+    def __init__(self, driver_path=r"C:\SeleniumDrivers", teardown=False):   
         self.driver_path = driver_path
-        #self.teardown = teardown
+        self.teardown = teardown
         os.environ['PATH'] += self.driver_path
         super(Searching, self).__init__()
         self.implicitly_wait(9)
         
 
-    #def __exit__(self, exc_type, exc_val, exc_tb):
-    #    if self.teardown:
-    #        self.quit()
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.teardown:
+            self.quit()
 
 
     def main_page(self):
