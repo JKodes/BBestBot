bc637044a2563b733148e9480b788b87ebf93642
unknown <jawanistanford@students.ptechnyc.org>
2022-11-13 22:35:22 -0500

    add new calls

--- run.py
+++ run.py
@@ -8,3 +8,7 @@ import searching.search_buy
 with searching.search_buy.Searching() as done:
     done.main_page()
     done.type_item_of_choice('Air Conditioner')
+    done.click_button()
+    done.add_to_cart()
+    done.go_to_cart()
+    done.checkout()
