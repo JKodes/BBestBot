0330eb81702cb038ec73f9bfe5961e6da0e90a95
unknown <jawanistanford@students.ptechnyc.org>
2022-11-17 23:35:34 -0500

    call exisitng customer email and password then sign in

--- run.py
+++ run.py
@@ -7,8 +7,10 @@ import searching.search_buy
 
 with searching.search_buy.Searching() as done:
     done.main_page()
-    done.type_item_of_choice('Air Conditioner')
+    done.type_item_of_choice('portable air Conditioner')
     done.click_button()
     done.add_to_cart()
     done.go_to_cart()
     done.checkout()
+    done.returning_customer_email('')  #email for people who already have an account, but not signed in
+    done.returning_customer_password('') #password for people who already have an account, but not signed in
